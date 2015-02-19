468d5ecf009079b89a02e797127011511134ceaf
Cory Flanigan <cory@lfo.audio>
2015-02-19 15:41:07 -0700

    Clean up config
    Wraps ENV access into a method that returns a single memoized hash

--- birdbath.rb
+++ birdbath.rb
@@ -3,17 +3,27 @@ require 'bundler/setup'
 
 require 'twitter'
 
+def config
+  @config ||= {
+    twitter_api_key:       ENV["TWITTER_API_KEY"],
+    twitter_api_secret:    ENV["TWITTER_API_SECRET"],
+    twitter_access_token:  ENV["TWITTER_ACCESS_TOKEN"],
+    twitter_access_secret: ENV["TWITTER_ACCESS_SECRET"],
+    twitter_username:      ENV["TWITTER_USERNAME"]
+  }
+end
+
 def client
-  @client ||= Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV["TWITTER_API_KEY"]
-    config.consumer_secret     = ENV["TWITTER_API_SECRET"]
-    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-    config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+  @client ||= Twitter::REST::Client.new do |client|
+    client.consumer_key        = config[:twitter_api_key]
+    client.consumer_secret     = config[:twitter_api_secret]
+    client.access_token        = config[:twitter_api_key]
+    client.access_token_secret = config[:twitter_access_secret]
   end
 end
 
 def username
-  ENV["TWITTER_USERNAME"]
+  config[:twitter_username]
 end
 
 def user
